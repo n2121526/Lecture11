fb5ad224f76fb35b3ce379a107d0b0b20d3a754d
Nargiz Ibragimova <nargizibragimova@MacBook-Pro-Nargiz-3.local>
2022-08-04 13:24:26 +0300

    vhfgbhjk

--- Example002_HelloUser/Program.cs
+++ Example002_HelloUser/Program.cs
@@ -1,3 +1,4 @@
-﻿string username = Console.ReadLine ();
-Console.WriteLine("Привет, ");
-Console.WriteLine(username);
+﻿Console.Write("Введите ваше имя ");
+string username = Console.ReadLine ();
+Console.Write("Привет, ");
+Console.Write(username);
